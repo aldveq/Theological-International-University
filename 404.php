02fc50c4517f9b205d964033d32869dc78281878
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2023-11-23 21:18:07 -0600

    added strings tranlation for 404 template

--- 404.php
+++ 404.php
@@ -14,10 +14,8 @@ get_header();
 		<section class="d-flex align-items-center justify-content-center">
 			<div class="text-center d-flex flex-column align-items-center justify-content-center">
 				<h1 class="fw-bold">404</h1>
-				<p class="fs-3 mb-1"> Opps! Page not found.</p>
-				<p class="lead mb-1">
-					The page you’re looking for doesn’t exist.
-				</p>
+				<p class="fs-3 mb-1"><?php esc_html_e( 'Oops! Page not found.', 'theological-international-university' ); ?></p>
+				<p class="lead mb-1"><?php esc_html_e( 'The page you’re looking for doesn’t exist.', 'theological-international-university' ); ?></p>
 				<div class="become_button text-center trans_200 mt-2">
 					<?php
 						$home_path = '';
@@ -28,11 +26,19 @@ get_header();
 						$home_path = home_url();
 						endif;
 
-					if ( is_user_logged_in() && current_user_can( 'tiu_student' ) ) :
+					if ( is_user_logged_in() 
+						&& current_user_can( 'tiu_student' ) 
+						&& pll_current_language( 'slug' ) === 'en' ) :
 						$home_path = home_url( '/student-dashboard/' );
 						endif;
+
+					if ( is_user_logged_in() 
+						&& current_user_can( 'tiu_student' ) 
+						&& pll_current_language( 'slug' ) === 'es' ) :
+						$home_path = home_url( '/panel-estudiantes/' );
+						endif;
 					?>
-					<a href="<?php echo esc_url( $home_path ); ?>" target="_self">Go Home</a>
+					<a href="<?php echo esc_url( $home_path ); ?>" target="_self"><?php esc_html_e( 'Go Home', 'theological-international-university' ); ?></a>
 				</div>
 			</div>
 		</section>
